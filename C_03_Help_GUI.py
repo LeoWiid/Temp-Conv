a507066ef44815f4691094f48005536e4c211e50
LeoWiid <139720063+LeoWiid@users.noreply.github.com>
2025-03-20 11:58:32 +1300

    Add files via upload

--- C_03_Help_GUI.py
+++ C_03_Help_GUI.py
@@ -1,21 +1,21 @@
 from tkinter import *
+from functools import partial  # To prevent unwanted windows
 
 
 class Converter:
     """
-    Temperature conversion tool (C to F or F to C)
+    Temperature conversion tool (°C to °F or °F to °C)
     """
 
     def __init__(self):
         """
         Temperature converter GUI
         """
-
         self.temp_frame = Frame(padx=10, pady=10)
         self.temp_frame.grid()
 
         self.to_help_button = Button(self.temp_frame,
-                                     text="Help / intro",
+                                     text="Help / Info",
                                      bg="#CC6600",
                                      fg="#FFFFFF",
                                      font=("Arial", "14", "bold"), width=12,
@@ -23,18 +23,31 @@ class Converter:
         self.to_help_button.grid(row=1, padx=5, pady=5)
 
     def to_help(self):
-        DisplayHelp()
+        """
+        Open help dialogue box and disables button
+        (so that users cant create multiple help boxes).
+        """
+        DisplayHelp(self)
+
 
 class DisplayHelp:
 
-    def __init__(self):
-
+    def __init__(self, partner):
         # setup dialogue box and background colour
         background = "#ffe6cc"
         self.help_box = Toplevel()
 
+        # disable help button
+        partner.to_help_button.config(state=DISABLED)
+
+        # If users press cross at top, closes help and
+        # 'release' help button
+        self.help_box.protocol('WM_DELETE_WINDOW',
+                               partial(self.close_help, partner))
+
         self.help_frame = Frame(self.help_box, width=300,
                                 height=200)
+
         self.help_frame.grid()
 
         self.help_heading_label = Label(self.help_frame,
@@ -42,35 +55,48 @@ class DisplayHelp:
                                         font=("Arial", "14", "bold"))
         self.help_heading_label.grid(row=0)
 
-        help_text = "To use the program, simply enter the temperature " \
+        help_text = "To yse the program, simply enter the temperature " \
                     "you wish to convert and then choose to convert " \
-                    " to either degrees Celsius (centigrade) or " \
+                    "to either degrees Celsius (centigrade) or " \
                     "Fahrenheit.. \n\n" \
-                    "Note that -273 degrees C " \
+                    " Note that -273 degrees C " \
                     "(-459 F) is absolute zero (the coldest possible " \
                     "temperature). If you try to convert a " \
-
+                    "temperature that is less than -273 degrees C, " \
+                    "you will get an error message. \n\n" \
+                    "To see your " \
+                    "calculation history and export it to a text " \
+                    "file, please click the 'History / Export' button."
 
         self.help_text_label = Label(self.help_frame,
-                                     text="help text goes here", wraplength=350,
+                                     text=help_text, wraplength=350,
                                      justify="left")
         self.help_text_label.grid(row=1, padx=10)
 
         self.dismiss_button = Button(self.help_frame,
                                      font=("Arial", "12", "bold"),
                                      text="Dismiss", bg="#CC6600",
-                                     fg="#FFFFFF", command=self.close_help)
+                                     fg="#FFFFFF",
+                                     command=partial(self.close_help, partner))
         self.dismiss_button.grid(row=2, padx=10, pady=10)
 
+        recolour_list = [self.help_frame, self.help_heading_label,
+                         self.help_text_label]
+
         # List and loop to set background colour on
-        # everything except the buttons
+        # everything except the buttons.
         recolour_list = [self.help_frame, self.help_heading_label,
                          self.help_text_label]
 
         for item in recolour_list:
             item.config(bg=background)
 
-    def close_help(self):
+    def close_help(self, partner):
+        """
+        Closes help dialogue box (and enables help button)
+        """
+        # Put help button back to normal...
+        partner.to_help_button.config(state=NORMAL)
         self.help_box.destroy()
 
 
